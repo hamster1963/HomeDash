a69ee0d817bc45b6a7a55abcfe508c3cb820bce7
hamster1963 <1410514192@qq.com>
2023-09-18 10:27:28 +0800

    feat: 适配移动端，在手机上打开默认收起导航栏

--- app/home/leftSide.tsx
+++ app/home/leftSide.tsx
@@ -8,7 +8,7 @@ import {
 } from "@douyinfe/semi-icons";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 const routerMap: Record<string, string> = {
   Home: "/home/main",
   Service: "/home/service",
@@ -31,17 +31,34 @@ function LeftSide({ callbackWidth }: LeftSideProps) {
   // 添加状态来控制导航栏的收缩和展开
   const [isNavCollapsed, setIsNavCollapsed] = useState(false);
 
-  const toggleNav = (isCollapse: boolean) => {
-    setIsNavCollapsed(isCollapse);
-    callbackWidth(isCollapse ? "60px" : "220px");
-  };
+  const toggleNav = useCallback(
+    (isCollapse: boolean) => {
+      setIsNavCollapsed(isCollapse);
+      callbackWidth(isCollapse ? "60px" : "220px");
+    },
+    [callbackWidth],
+  );
 
   // 根据导航栏的收缩状态来设置宽度
   const navWidth = isNavCollapsed ? 60 : 220;
 
+  // 监测窗口大小变化，如果窗口宽度小于 768px，导航栏自动收缩
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        toggleNav(true);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [toggleNav]);
+
   return (
     <div style={{ position: "fixed", top: 0, left: 0, bottom: 0 }}>
       <Nav
+        isCollapsed={isNavCollapsed}
         onCollapseChange={(isCollapse) => toggleNav(isCollapse)}
         className={"my-nav"}
         style={{ height: "100vh", width: navWidth }}
