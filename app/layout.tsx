79f3ad7c361af6c35fb2b098c220bc85ca641ac0
hamster1963 <1410514192@qq.com>
2024-01-09 17:39:05 +0800

    feat: 新增 umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import React from "react";
 
 import { SSEConnectProvider } from "@/app/home/utils/sseContext";
@@ -15,6 +15,10 @@ export default function RootLayout({
   return (
     <html>
       <body className={inter.className}>
+        <Script
+          src="https://umami.buycoffee.top/script.js"
+          data-website-id="d51d1579-2f3e-4d7d-9172-a860c016ec58"
+        />
         <Providers>
           <SSEConnectProvider>{children}</SSEConnectProvider>
         </Providers>
